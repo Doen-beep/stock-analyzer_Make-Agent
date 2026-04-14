938f392a9c87327cde7765f669cbd58fa8a9265c
Doen-beep <denis2buc@gmail.com>
2026-04-14 21:25:45 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,5 @@
 const WEBHOOK    = 'https://hook.eu1.make.com/1mlw60agtu0lw22kupuqvxa6jfwsx1oa/';
 const WEBHOOK_AI = 'https://hook.eu1.make.com/xhpvfxz13qin7xhr0jx92v5owusxidys';
+
+// URL API Yahoo Finance — endpoint Vercel (yahoo-finance2, sans quota)
+const WEBHOOK = '/api/finance';
