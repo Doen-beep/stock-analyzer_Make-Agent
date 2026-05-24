ebdcb6a631f83eb30192566b11f311406743892a
Doen-beep <denis2buc@gmail.com>
2026-05-24 17:53:57 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-/* config.js | v1.0 | 2026-05-24 */
-// URL API Yahoo Finance — Make → RapidAPI
-const WEBHOOK = 'https://hook.eu1.make.com/1mlw60agtu0lw22kupuqvxa6jfwsx1oa';
-
-// URL Analyse IA — Make → OpenAI
-const WEBHOOK_AI = 'https://hook.eu1.make.com/xhpvfxz13qin7xhr0jx92v5owusxidys';
