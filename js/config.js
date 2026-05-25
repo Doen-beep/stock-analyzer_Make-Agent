2d90d904f31bbf2e26bcdbac198da3e5f96e4c99
Doen-beep <denis2buc@gmail.com>
2026-05-25 09:02:44 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,9 @@
-/* config.js | v1.0 | 2026-05-24 */
+/* config.js | v1.1 | 2026-05-25 */
 // URL API Yahoo Finance — Make → RapidAPI
 const WEBHOOK = 'https://hook.eu1.make.com/1mlw60agtu0lw22kupuqvxa6jfwsx1oa';
 
 // URL Analyse IA — Make → OpenAI
 const WEBHOOK_AI = 'https://hook.eu1.make.com/xhpvfxz13qin7xhr0jx92v5owusxidys';
+
+// Base URL Vercel
+const VERCEL_URL = 'https://stock-analyzer-make-agent.vercel.app';
